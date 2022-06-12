7d438794500b9702dd6ab5c2cc407ec8d3007c29
Mukunda Ramanna <mram102460@taconic.com>
2022-06-12 18:10:48 +0200

    Changes to schema.yml

--- greenery/models/staging/stg_addresses.sql
+++ greenery/models/staging/stg_addresses.sql
@@ -5,17 +5,9 @@
 }}
 
 select
-  order_id,
-	user_id,
-	promo_id,
 	address_id,
-	created_at,
-	order_cost,
-	shipping_cost,
-	order_total,
-	tracking_id,
-	shipping_service,
-	estimated_delivery_at,
-	delivered_at,
-	status
-from {{source('greenery_source','orders')}}
+	address,
+	zipcode,
+	state,
+	country
+from {{source('greenery_source','addresses')}}
--- greenery/models/staging/stg_promos.sql
+++ greenery/models/staging/stg_promos.sql
@@ -4,18 +4,9 @@
   )
 }}
 
+
 select
-  order_id,
-	user_id,
 	promo_id,
-	address_id,
-	created_at,
-	order_cost,
-	shipping_cost,
-	order_total,
-	tracking_id,
-	shipping_service,
-	estimated_delivery_at,
-	delivered_at,
+	discount,
 	status
-from {{source('greenery_source','orders')}}
+from {{source('greenery_source','promos')}}
